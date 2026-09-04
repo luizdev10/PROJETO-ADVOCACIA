0ca691105c811258b3d6d4414f6e88dcb139eadf
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-09-03 22:47:33 -0300

    estilização da page de inicio e atuação

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Inicio from "./paginas/inicio.jsx";
 import Navbar from "./paginas/navbar.jsx";
+import Atuacao from "./paginas/atuacao.jsx";
 
 function App() {
   return (
     <>
     <Navbar />
     <Inicio />
+    <Atuacao />
     </>
   );
 }
--- src/paginas/inicio.jsx
+++ src/paginas/inicio.jsx
@@ -20,26 +20,26 @@ export default function Inicio() {
   return (
     <>
 
-      <section className=" h-screen overflow-hidden pt-20 flex flex-col justify-center">
-        <img className="absolute inset-0 w-full h-full object-cove" src={Photo} alt="" />
-        <div class="absolute inset-0 bg-black/40"></div>
+      <section className=" bg-gray-800 h-screen overflow-hidden pt-20 flex flex-col justify-center">
+     
         <div className="p-10 relative z-10 text-white">
-          <h1 className="text-4xl">Soluções Jurídicas Ágeis.</h1>
-          <p className="text-xl pt-7">Escritório de advocacia atuante em causas Previdenciárias, trabalhistas, causas cíveis e criminais.</p>
+          <h1 className="text-5xl font-[Roboto] text-amber-500 text-center">Soluções Jurídicas Ágeis</h1>
+          <p className="text-xl pt-7 font-light text-center">Escritório de advocacia atuante em causas Previdenciárias, trabalhistas, causas cíveis e criminais.</p>
         </div>
         <div className="z-10 flex justify-center items-center">
-          <div className=" flex gap-2 bg-amber-950 rounded-full px-6 py-2 font-semibold text-white cursor-pointer ">
+          <div className="flex gap-2 bg-amber-700 rounded-full px-6 py-2 font-semibold text-white absolute cursor-pointer transition-all border-b-2 border-amber-950 hover:bg-amber-600 hover:border-b-4 hover:border-amber-800">
             <MessageCircleMore />
-            <a href="">Agendar Consulta Jurídica</a>
+            <a href="" className="font-light">Agendar Consulta Jurídica</a>
           </div>
         </div>
 
         <div className="mt-12 z-10 flex flex-wrap justify-center items-center gap-3 text-white font-medium">
           {quality.map(item => (
-            <div className="flex items-center gap-2">
+            <div key={item.id}
+            className="flex items-center gap-2">
               <div className="w-2 h-2 bg-amber-200 rounded-full flex justify-center items-center">
               </div>
-              <span className="text-amber-100">{item.description}</span>
+              <span className="text-white font-light">{item.description}</span>
             </div>
           ))}
         </div>
